0c327586cfb9dea69a58429b4e5a6604fe062469
codingonapotato <hcpc215@gmail.com>
2024-11-18 18:22:58 -0800

    created script to generate filtered manifest based on filtered metadata file input

---
+++ r/generate_updated_manifest.R
@@ -0,0 +1,16 @@
+# Purpose: Generates an updated manifest file that is a subset of the original manifest file based on the sample ID's in the supplied metadata file
+library(tidyverse)
+
+#### Load manifest and final filtered metadata ####
+pd_manifest <- read_tsv("parkinsons_manifest.txt")
+filtered_final_metadata <- read_tsv("pd_filtered_final_metadata.tsv")
+
+#### Subset pd_manifest such that only rows with sample ID in filtered_final_metadata are included ####
+# Get vector of all sample ID's present in the filtered_final_metadata
+samples <- filtered_final_metadata$`#SampleID`
+# Subset pd_manifest
+subset_pd_manifest <- pd_manifest %>% filter(`sample-id` %in% samples)
+
+#### Save to file ####
+export_file_path <- "filtered_parkinsons_manifest.tsv"
+write_tsv(subset_pd_manifest, export_file_path)
