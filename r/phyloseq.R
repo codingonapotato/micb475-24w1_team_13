3644b1f1ecc9ddfb9547d122f12804829c664470
codingonapotato <hcpc215@gmail.com>
2024-11-24 13:35:00 -0800

    [1] Updated phyloseq.R and saved phyloseq object [2] Removed r/aim2/data

--- r/phyloseq.R
+++ r/phyloseq.R
@@ -6,13 +6,13 @@ library(vegan)
 #### Load data ####
 # Change file paths as necessary
 metafp <- "data/pd_filtered_metadata.txt"
-meta <- read_delim(metafp, delim="\t")
+meta <- read_delim(metafp, delim = "\t")
 
 otufp <- "data/feature-table.txt"
-otu <- read_delim(file = otufp, delim="\t", skip=1)
+otu <- read_delim(file = otufp, delim = "\t", skip = 1)
 
 taxfp <- "data/taxonomy.tsv"
-tax <- read_delim(taxfp, delim="\t")
+tax <- read_delim(taxfp, delim = "\t")
 
 phylotreefp <- "data/tree.nwk"
 phylotree <- read.tree(phylotreefp)
@@ -23,30 +23,33 @@ phylotree <- read.tree(phylotreefp)
 # Note: tibbles do not allow rownames so if you imported with read_delim, change back
 
 # save everything except first column (OTU ID) into a matrix
-otu_mat <- as.matrix(otu[,-1])
+otu_mat <- as.matrix(otu[, -1])
 # Make first column (#OTU ID) the rownames of the new matrix
 rownames(otu_mat) <- otu$`#OTU ID`
 # Use the "otu_table" function to make an OTU table
-OTU <- otu_table(otu_mat, taxa_are_rows = TRUE) 
+OTU <- otu_table(otu_mat, taxa_are_rows = TRUE)
 class(OTU)
 
 #### Format sample metadata ####
 # Save everything except sampleid as new data frame
-samp_df <- as.data.frame(meta[,-1])
+samp_df <- as.data.frame(meta[, -1])
 # Make sampleids the rownames
-rownames(samp_df)<- meta$'#SampleID'
+rownames(samp_df) <- meta$"#SampleID"
 # Make phyloseq sample data with sample_data() function
 SAMP <- sample_data(samp_df)
 class(SAMP)
 
 #### Formatting taxonomy ####
 # Convert taxon strings to a table with separate taxa rank columns
-tax_mat <- tax %>% select(-Confidence)%>%
-  separate(col=Taxon, sep="; "
-           , into = c("Domain","Phylum","Class","Order","Family","Genus","Species")) %>%
+tax_mat <- tax %>%
+  select(-Confidence) %>%
+  separate(
+    col = Taxon, sep = "; ",
+    into = c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species")
+  ) %>%
   as.matrix() # Saving as a matrix
-# Save everything except feature IDs 
-tax_mat <- tax_mat[,-1]
+# Save everything except feature IDs
+tax_mat <- tax_mat[, -1]
 # Make sampleids the rownames
 rownames(tax_mat) <- tax$`Feature ID`
 # Make taxa table
@@ -63,3 +66,9 @@ otu_table(depression)
 sample_data(depression)
 tax_table(depression)
 phy_tree(depression)
+
+#### Quality Control ####
+# TODO(?)
+
+#### Save phyloseq object ####
+save(depression, file = "data/depression_phyloseq.RData")
