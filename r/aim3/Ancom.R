2a7f94a13d0f6a5ecc094e73ac2af3ba171f4b48
Ivie <76536831+iv863@users.noreply.github.com>
2024-11-24 18:08:39 -0800

    Added ancom analysis

---
+++ r/aim3/Ancom.R
@@ -0,0 +1,91 @@
+library(tidyverse)
+library(phyloseq) 
+library(ggpubr)
+library(randomcoloR)
+library(ANCOMBC)
+library(microbiome)
+
+load("depression_phyloseq.Rdata")
+
+family = tax_glom(depression,'Family')
+ntaxa(depression); ntaxa(family)
+
+calculate_relative_abundance <- function(x) x / sum(x)
+total_counts <- taxa_sums(family)
+relative_abundance <- calculate_relative_abundance(total_counts)
+abundant <- relative_abundance > 0.001
+family <- prune_taxa(abundant, family)
+family
+
+# Ensure the grouping variable is set correctly
+sample_data(family)$BDI_category_antidepressant_use <- factor(
+  sample_data(family)$BDI_category_antidepressant_use, 
+  levels = c("low+no", "low+yes", "high+no", "high+yes")
+)
+
+ancom.family = ancombc(phyloseq = family,
+                       formula = 'BDI_category_antidepressant_use',
+                       p_adj_method = "fdr",
+                       prv_cut=0.10,
+                       lib_cut = 1000,
+                       group = 'BDI_category_antidepressant_use',
+                       struc_zero = T)
+
+colnames(ancom.family$res$lfc) = paste(colnames(ancom.family$res$lfc),'_beta',sep='')
+colnames(ancom.family$res$se) = paste(colnames(ancom.family$res$se),'_se',sep='')
+colnames(ancom.family$res$W) = paste(colnames(ancom.family$res$W),'_W',sep='')
+colnames(ancom.family$res$p_val) = paste(colnames(ancom.family$res$p_val),'_p_val',sep='')
+colnames(ancom.family$res$q_val) = paste(colnames(ancom.family$res$q_val),'_q_val',sep='')
+colnames(ancom.family$res$diff_abn) = paste(colnames(ancom.family$res$diff_abn),'_diff_abn',sep='')
+
+results = lapply(ancom.family$res,function(x) rownames_to_column(x,'Family')) %>% 
+  lapply(as_tibble) %>% reduce(full_join)
+
+results = results %>% dplyr::select(-contains('Intercept'))
+
+srv = Vectorize(str_remove)
+colnames(results) = srv(colnames(results),'Fractionpos_')
+colnames(results)
+
+# Plotting the results
+q_val_columns <- grep("_q_val$", colnames(results), value = TRUE)
+results.sig = results %>%
+  filter(if_any(all_of(q_val_columns), ~ . < 0.05))
+
+hits = results.sig$Family
+hits = as.numeric(hits)
+
+family_tss = family %>% transform('compositional')
+selected_taxa <- taxa_names(family)[hits]
+family_tss <- prune_taxa(selected_taxa, family_tss)
+family_tss_melt = family_tss %>% psmelt()
+
+bug = unique(family_tss_melt$Family)
+family_tss_melt = family_tss_melt %>% 
+  dplyr::select(-c(OTU,Domain:Order)) %>%
+  pivot_wider(names_from = Family, values_from = Abundance)
+
+for(b in bug){
+    p = family_tss_melt %>% 
+    mutate(BDI_Category_Antidepressant_Use = str_to_title(BDI_category_antidepressant_use))
+    
+    colors <- randomColor(4)
+  
+  ggplot(p,aes(x = BDI_category_antidepressant_use, y = .data[[b]],fill=BDI_category_antidepressant_use)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_jitter(height = 0, width = 0.2) +
+    theme_classic(base_size = 16) +
+    theme(legend.position='none') +
+    scale_fill_manual(values = colors) +
+    xlab('BDI_category_antidepressant_use') + ylab(paste(b,'(% Ab.)',sep=' '))
+  print(p)
+  ggsave(paste('tss_',b,'.jpeg',sep=''),height = 5,width = 5)
+}
+
+saveRDS(ancom.family,'ancom_results_family.rds')
+
+write.csv(results,'ancom_results_family.csv',row.names = F)
+
+library(writexl)
+write_xlsx(list('all_results' = results,'sig_results' = results.sig),
+           'ancom_results_family.xlsx')
