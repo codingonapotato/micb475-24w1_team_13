04e695e1b30a8d8dc2cf91d7b2a8d6fde587c8d5
codingonapotato <hcpc215@gmail.com>
2024-11-25 17:39:29 -0800

    Updated indicator_species_analysis.R script to generate and export tables of ISA results for antidepressant use, bdi_category, and bdi_category+antidepressant_use categories and export as .RData objects

--- r/indicator_species_analysis.R
+++ r/indicator_species_analysis.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(phyloseq)
 library(indicspecies)
-library(ggpubr)
 
 #### Load Data ####
 load("data/depression_phyloseq.RData")
@@ -11,19 +10,42 @@ load("data/depression_phyloseq.RData")
 depression_species <- tax_glom(depression, "Species", NArm = TRUE)
 depression_species_RA <- transform_sample_counts(depression_species, fun = function(x) x / sum(x))
 
-# ISA
-isa_depression <- multipatt(t(otu_table(depression_species_RA)), cluster = sample_data(depression_species_RA)$BDI_category)
-summary(isa_depression)
+# Modify taxnomy table
 taxtable <- tax_table(depression_species) %>%
   as.data.frame() %>%
   rownames_to_column(var = "ASV")
 
-isa_final <- isa_depression$sign %>%
+# ISA BDI Category
+isa_bdi_category <- multipatt(t(otu_table(depression_species_RA)), 
+                              cluster = sample_data(depression_species_RA)$BDI_category)
+summary(isa_bdi_category)
+
+isa_final_bdi_category <- isa_bdi_category$sign %>%
   rownames_to_column(var = "ASV") %>%
   left_join(taxtable) %>%
   filter(p.value < 0.05)
 
-isa_final %>% View()
+# ISA Antidepressant_use
+isa_antidepressant <- multipatt(t(otu_table(depression_species_RA)), 
+                              cluster = sample_data(depression_species_RA)$Antidepressant_use)
+summary(isa_antidepressant)
+
+isa_final_antidepressant <- isa_antidepressant$sign %>%
+  rownames_to_column(var = "ASV") %>%
+  left_join(taxtable) %>%
+  filter(p.value < 0.05)
+
+# ISA Antidepressant_use and BDI_category
+isa_mixed <- multipatt(t(otu_table(depression_species_RA)), 
+                       cluster = sample_data(depression_species_RA)$BDI_category_antidepressant_use)
+summary(isa_mixed)
+
+isa_final_mixed <- isa_mixed$sign %>%
+  rownames_to_column(var = "ASV") %>%
+  left_join(taxtable) %>%
+  filter(p.value < 0.05)
 
 #### Save data ####
-save(isa_final, file = "data/isa_final.RData")
+save(isa_final_bdi_category, file = "data/isa_final_bdi_category.RData")
+save(isa_final_antidepressant, file = "data/isa_final_antidepressant.RData")
+save(isa_final_mixed, file = "data/isa_final_mixed")
