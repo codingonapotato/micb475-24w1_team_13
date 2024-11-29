b495cb4cf13fbc9c218eb34f1e2ba402849a4af1
codingonapotato <hcpc215@gmail.com>
2024-11-28 16:03:57 -0800

    Added load lines for isa analysis + fixed bug where file extension was missing for isa_final_mixed

--- r/indicator_species_analysis.R
+++ r/indicator_species_analysis.R
@@ -48,4 +48,9 @@ isa_final_mixed <- isa_mixed$sign %>%
 #### Save data ####
 save(isa_final_bdi_category, file = "data/isa_final_bdi_category.RData")
 save(isa_final_antidepressant, file = "data/isa_final_antidepressant.RData")
-save(isa_final_mixed, file = "data/isa_final_mixed")
+save(isa_final_mixed, file = "data/isa_final_mixed.RData")
+
+#### Load data ####
+load("data/isa_final_bdi_category.RData")
+load("data/isa_final_antidepressant.RData")
+load("data/isa_final_mixed.RData")
